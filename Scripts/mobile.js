2c00336d63748a228cb572dc3071cc224424885f
dillonsimplebiz <113392704+dillonsimplebiz@users.noreply.github.com>
2023-04-06 15:17:19 -0400

    workplace-partners-v2

--- Scripts/mobile.js
+++ Scripts/mobile.js
@@ -3,19 +3,18 @@
     window.Parameters = window.Parameters || {
         AjaxContainer: 'div.dmBody',
         WrappingContainer: 'div.dmOuter',
-        HomeUrl: 'http://app.multiscreensite.com/site/22043599',
+        HomeUrl: 'http://app.multiscreensite.com/site/d6583fbf',
         AccountUUID: '3d875b9e5c88468a9855df44ab3479f3',
         SystemID: 'US_DIRECT_PRODUCTION',
-        SiteAlias: '22043599',
-        SiteId: '2481767',
+        SiteAlias: 'd6583fbf',
         SiteType: atob('RFVEQU9ORQ=='),
-        PublicationDate: 'Thu Jun 23 19:13:20 UTC 2022',
+        PublicationDate: 'Tue Jan 31 18:15:57 UTC 2023',
         ExternalUid: null,
         IsSiteMultilingual: false,
         InitialPostAlias: '',
         InitialDynamicItem: '',
         InitialPageAlias: 'home',
-        InitialPageUuid: '18269d6d6f164e71bf1df588275f8183',
+        InitialPageUuid: 'de9444bf1cb1450aa5b01fab20a80071',
         InitialEncodedPageAlias: 'aG9tZQ==',
         CurrentPageUrl: '',
         IsCurrentHomePage: true,
@@ -24,7 +23,7 @@
         HomeLinkText: 'Back To Home',
         UseGalleryModule: false,
         CurrentThemeName: 'Layout Theme',
-        ThemeVersion: '6370',
+        ThemeVersion: '8720',
         DefaultPageAlias: '',
         RemoveDID: true,
         WidgetStyleID: null,
@@ -35,19 +34,21 @@
         StorePagesUrls: 'e30=',
         IsNewStore: 'false',
         StorePath: '',
-        StoreId: 'null',
-        StoreVersion: 0,
-        StoreBaseUrl: '/site/22043599?preview=true&dm_device=mobile&dm_exportSite=true&nossl&dm_exportSite_protected=cf30988b_1656011648366_5_1dcbfec2a492881a6f6a2155999d1e5eeab2f4822dfa396f0f3f6088827e4e46',
+        StoreId: '80021013',
+        StoreVersion: 2,
+        StoreBaseUrl: '/site/d6583fbf?preview=true&dm_device=mobile&dm_exportSite=true&nossl&dm_exportSite_protected=faf88b8c_1680806856985_5_5347c86ce18eb1452a8ba414876f4b9b5e59f41d99c0679cf754651db16e6aab',
         StoreCleanUrl: true,
         StoreDisableScrolling: true,
-        NotificationSubDomain: 'thewindowguytn',
+        IsStoreSuspended: false,
+        NotificationSubDomain: 'yourworkplacepartners',
         HasCustomDomain: true,
         showCookieNotification: false,
         cookiesNotificationMarkup: 'null',
         translatedPageUrl: '',
         isFastMigrationSite: false,
-        sidebarPosition: 'NA',
+        sidebarPosition: 'LEFT',
         currentLanguage: 'en',
+        currentLocale: 'en',
         NavItems: '{}',
         errors: {
             general: 'There was an error connecting to the page.<br/> Make sure you are not offline.',
@@ -57,7 +58,7 @@
         NavigationAreaParams: {
             ShowBackToHomeOnInnerPages: true,
             NavbarSize: 5,
-            NavbarLiveHomePage: 'http://app.multiscreensite.com/site/22043599',
+            NavbarLiveHomePage: 'http://app.multiscreensite.com/site/d6583fbf',
             BlockContainerSelector: '.dmBody',
             NavbarSelector: '#dmNav:has(a)',
             SubNavbarSelector: '#subnav_main'
@@ -70,7 +71,7 @@
         disableTracking: false,
         pageType: 'FROM_SCRATCH',
         isRuntimeServer: true,
-        siteCacheKey: '1656011647070_22043599',
+        isInEditor: false,
         siteInfo: null,
     };
 
@@ -98,42 +99,110 @@
         }
     }
 
-    function loadCSS(link) {
-        try {
-            var urlParams = new URLSearchParams(window.location.search);
-            var noCSS = !!urlParams.get('nocss');
-            var cssTimeout = urlParams.get('cssTimeout') || 0;
 
-            if (noCSS) {
-                return;
-            }
-            requestIdleCallback(function () {
-                window.setTimeout(function () {
-                    link.onload = null;
-                    link.rel = 'stylesheet';
-                    link.type = 'text/css'
-                }, parseInt(cssTimeout, 10));
-            });
-        } catch (e) {/* Never fail - this is just a tool for measurements */}
+(function () {
+  let cssLinks = {};
+  function loadCssLink(link) {
+    link.onload = null;
+    link.rel = "stylesheet";
+    link.type = "text/css";
+  }
+
+  function checkCss() {
+    if (cssLinks && cssLinks.runtime && cssLinks.global) {
+      loadCssLink(cssLinks.runtime);
+      loadCssLink(cssLinks.global);
+      cssLinks = null;
     }
+  }
 
+  function loadCSS(link) {
+    try {
+      var urlParams = new URLSearchParams(window.location.search);
+      var noCSS = !!urlParams.get("nocss");
+      var cssTimeout = urlParams.get("cssTimeout") || 0;
 
-
-    function loadCSS(link) {
-        try {
-            var urlParams = new URLSearchParams(window.location.search);
-            var noCSS = !!urlParams.get('nocss');
-            var cssTimeout = urlParams.get('cssTimeout') || 0;
-
-            if (noCSS) {
-                return;
-            }
-            requestIdleCallback(function () {
-                window.setTimeout(function () {
-                    link.onload = null;
-                    link.rel = 'stylesheet';
-                    link.type = 'text/css'
-                }, parseInt(cssTimeout, 10));
-            });
-        } catch (e) {/* Never fail - this is just a tool for measurements */}
+      if (noCSS) {
+        return;
+      }
+      if (link.href.includes("d-css-runtime")) {
+        cssLinks.runtime = link;
+        checkCss();
+      } else if (link.id === "siteGlobalCss") {
+        cssLinks.global = link;
+        checkCss();
+      } else {
+        requestIdleCallback(function () {
+          window.setTimeout(function () {
+            loadCssLink(link);
+          }, parseInt(cssTimeout, 10));
+        });
+      }
+    } catch (e) {
+      /* Never fail - this is just a tool for measurements */
     }
+  }
+  window.loadCSS = window.loadCSS || loadCSS;
+})();
+
+
+    /* usage: window.getDeferred(<deferred name>).resolve() or window.getDeferred(<deferred name>).promise.then(...)*/
+    function Def(){this.promise=new Promise((function(a,b){this.resolve=a,this.reject=b}).bind(this))}
+    const defs={};
+    window.getDeferred=function(a){return null==defs[a]&&(defs[a]=new Def),defs[a]}
+    window.waitForDeferred = function (b, a, c) {
+      let d = window?.getDeferred?.(b);
+      d
+        ? d.promise.then(a)
+        : c && ["complete", "interactive"].includes(document.readyState)
+        ? setTimeout(a, 1)
+        : c
+        ? document.addEventListener("DOMContentLoaded", a)
+        : console.error(`Deferred  does not exist`);
+    };
+
+
+(function () {
+  let cssLinks = {};
+  function loadCssLink(link) {
+    link.onload = null;
+    link.rel = "stylesheet";
+    link.type = "text/css";
+  }
+
+  function checkCss() {
+    if (cssLinks && cssLinks.runtime && cssLinks.global) {
+      loadCssLink(cssLinks.runtime);
+      loadCssLink(cssLinks.global);
+      cssLinks = null;
+    }
+  }
+
+  function loadCSS(link) {
+    try {
+      var urlParams = new URLSearchParams(window.location.search);
+      var noCSS = !!urlParams.get("nocss");
+      var cssTimeout = urlParams.get("cssTimeout") || 0;
+
+      if (noCSS) {
+        return;
+      }
+      if (link.href.includes("d-css-runtime")) {
+        cssLinks.runtime = link;
+        checkCss();
+      } else if (link.id === "siteGlobalCss") {
+        cssLinks.global = link;
+        checkCss();
+      } else {
+        requestIdleCallback(function () {
+          window.setTimeout(function () {
+            loadCssLink(link);
+          }, parseInt(cssTimeout, 10));
+        });
+      }
+    } catch (e) {
+      /* Never fail - this is just a tool for measurements */
+    }
+  }
+  window.loadCSS = window.loadCSS || loadCSS;
+})();

15da736f07b43ff73127fed1c70724b6034adbb7
dillonsimplebiz <113392704+dillonsimplebiz@users.noreply.github.com>
2023-04-21 13:53:14 -0400

    Update popup.js

--- bitrix_lookup/popup.js
+++ bitrix_lookup/popup.js
@@ -1,8 +1,9 @@
 function main() {
-  document.querySelector("#upda").innerHTML = '1:25'
+  document.querySelector("#upda").innerHTML = '1:52'
   //os.envitron['BITRIXTEST3']v4::
   auth = 'none_log'
   try{auth = process.env.BITRIXTEST}catch{del=1}
+  try{auth = config.BITRIXTEST}catch{del=1}
   try{auth = $BITRIXTEST3}catch{del=1}	
   console.log(auth)
 /*  try{auth = ${BITRIXTEST}}catch{del=1}

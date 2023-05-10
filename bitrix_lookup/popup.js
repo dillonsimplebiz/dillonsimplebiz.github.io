c570d17f63f12c320198c8b56dfbf35e44a62a3c
dillonsimplebiz <113392704+dillonsimplebiz@users.noreply.github.com>
2023-05-10 08:47:02 -0400

    Update popup.js

--- bitrix_lookup/popup.js
+++ bitrix_lookup/popup.js
@@ -198,8 +198,7 @@ $.when(
               lookuptablereviews.innerHTML = data
 		var tryagainst = document.getElementById('TITLE').innerHTML
 		var trs = document.getElementsByTagName('tr')
-		document.getElementById('sjr').innerHTML = 'Loading...'
-		document.getElementById('tpr').innerHTML = 'Loading...'	
+	
 		for (var i=0;i<trs.length;i++){
 			try{
 				if (trs[i].children[3].innerHTML.trim().includes(tryagainst.trim()) || tryagainst.trim().includes(trs[i].children[3].innerHTML.trim())){
@@ -228,6 +227,8 @@ $.when(
 		
 }
 function show(indexCover) {
+  document.getElementById('sjr').innerHTML = 'Loading...'
+  document.getElementById('tpr').innerHTML = 'Loading...'	
   index = indexCover.currentTarget.param
   table.style = 'display:none'
   table1.style = 'display:block'
